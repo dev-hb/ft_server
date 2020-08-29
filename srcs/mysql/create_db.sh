8902f03d932f73988980eda0daf5033d23db437a
edithturn <edithpuclla20@gmail.com>
2020-08-29 15:28:49 -0700

    clean query script

--- srcs/mysql/create_db.sh
+++ srcs/mysql/create_db.sh
@@ -1,20 +1,13 @@
 service mysql start
 # Creating DB
-echo "CREATE DATABASE db_wordpress;" | mysql -u root
-echo "CREATE USER 'admin' identified by 'adminpsw';" | mysql -u root
-echo "GRANT USAGE ON db_wordpress.* TO 'admin'@'localhost' identified by 'adminpsw';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON db_wordpress.* TO 'admin'@'localhost' identified by 'adminpsw';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
-
-
-# Creating Table and Data
-# service mysql start
-# echo "mysql -u admin -p db_wordpress;" | mysql -u root
-# echo "USE db_wordpress;"  | mysql -u admin
-# echo "CREATE TABLE students (id MEDIUMINT NOT NULL AUTO_INCREMENT, firstname CHAR(30) NOT NULL, PRIMARY KEY (id) );"  | mysql -u admin
-# echo "INSERT INTO students(firstname) VALUES ('Edith'), ('Zakaria');"  | mysql -u admin
-
-
+mysql --user=root --password=toor << EOF
+CREATE DATABASE db_wordpress;
+CREATE USER 'admin' identified by 'adminpsw';
+GRANT USAGE ON db_wordpress.* TO 'admin'@'localhost' identified by 'adminpsw';
+GRANT ALL PRIVILEGES ON db_wordpress.* TO 'admin'@'localhost' identified by 'adminpsw';
+FLUSH PRIVILEGES;
+EOF
+# Creating DB and Data
 mysql --user=admin --password=adminpsw db_wordpress << EOF
 CREATE TABLE students (id MEDIUMINT NOT NULL AUTO_INCREMENT, firstname CHAR(30) NOT NULL, PRIMARY KEY (id) );
 INSERT INTO students (firstname) VALUES ("Edith");
--- srcs/mysql/db_create_new_user.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE db_ft_server;
-USE db_ft_server
-CREATE TABLE students (
-     id MEDIUMINT NOT NULL AUTO_INCREMENT,
-     name CHAR(30) NOT NULL,
-     PRIMARY KEY (id)
-);
-INSERT INTO students(name) VALUES ('Edith'), ('Zakaria');
-exit
